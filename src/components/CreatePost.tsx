ae95e5b53de76c1e07bda7e4fb79284830bd562c
he4a00 <he4a00@github.com>
2023-05-24 15:47:09 +0300

    can't post unless you logged in

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -22,24 +22,36 @@ const CreatePost = () => {
     mutate({ content: postContent });
   };
   return (
-    <form
-      onSubmit={handleSubmit}
-      className=" flex flex-grow flex-col gap-2 border-b px-3 py-4"
-    >
-      <div className="flex gap-4">
-        <ProfileImage width={50} height={50} src={session?.user.image || ""} />
-        <textarea
-          className=" flex-grow resize-none outline-none"
-          placeholder="What's happening"
-          value={postContent}
-          onChange={(e) => setPostContent(e.target.value)}
-        />
-      </div>
-      <Button
-        className="self-end"
-        disabled={postContent.length === 0 || isLoading}
-      />
-    </form>
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className=" flex flex-grow flex-col gap-2 border-b px-3 py-4"
+      >
+        {session ? (
+          <>
+            <div className="flex gap-4">
+              <ProfileImage
+                width={50}
+                height={50}
+                src={session?.user.image || ""}
+              />
+              <textarea
+                className=" flex-grow resize-none outline-none"
+                placeholder="What's happening"
+                value={postContent}
+                onChange={(e) => setPostContent(e.target.value)}
+              />
+            </div>
+            <Button
+              className="self-end"
+              disabled={postContent.length === 0 || isLoading}
+            />{" "}
+          </>
+        ) : (
+          ""
+        )}
+      </form>
+    </>
   );
 };
 
